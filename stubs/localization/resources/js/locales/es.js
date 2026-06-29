98904435b6706b87abcc8e0a22a8767a2b5a06c8
kallbuloso <kallbuloso@gmail.com>
2026-06-29 14:52:03 -0300

    Adiciona traduções para os arquivos de localização em inglês, espanhol e português brasileiro

--- stubs/localization/resources/js/locales/es.js
+++ stubs/localization/resources/js/locales/es.js
@@ -1,1 +1,180 @@
-export { default } from 'element-plus/es/locale/lang/es'
+export default {
+    name: "es",
+    el: {
+        breadcrumb: { label: "Ruta de navegación" },
+        colorpicker: {
+            confirm: "Confirmar",
+            clear: "Limpiar",
+            defaultLabel: "selector de color",
+            description: "el color actual es {color}. pulsa Enter para seleccionar un nuevo color.",
+            alphaLabel: "seleccionar el valor alfa",
+            alphaDescription: "alfa {alpha}, el color actual es {color}",
+            hueLabel: "seleccionar el valor de matiz",
+            hueDescription: "matiz {hue}, el color actual es {color}",
+            svLabel: "seleccionar los valores de saturación y brillo",
+            svDescription: "saturación {saturation}, brillo {brightness}, el color actual es {color}",
+            predefineDescription: "seleccionar {value} como color"
+        },
+        datepicker: {
+            now: "Ahora",
+            today: "Hoy",
+            cancel: "Cancelar",
+            clear: "Limpiar",
+            confirm: "Confirmar",
+            dateTablePrompt: "Usa las teclas de flecha y Enter para seleccionar el día del mes",
+            monthTablePrompt: "Usa las teclas de flecha y Enter para seleccionar el mes",
+            yearTablePrompt: "Usa las teclas de flecha y Enter para seleccionar el año",
+            selectedDate: "Fecha seleccionada",
+            selectDate: "Seleccionar fecha",
+            selectTime: "Seleccionar hora",
+            startDate: "Fecha inicial",
+            startTime: "Hora inicial",
+            endDate: "Fecha final",
+            endTime: "Hora final",
+            prevYear: "Año anterior",
+            nextYear: "Año siguiente",
+            prevMonth: "Mes anterior",
+            nextMonth: "Mes siguiente",
+            year: "Año",
+            month1: "enero",
+            month2: "febrero",
+            month3: "marzo",
+            month4: "abril",
+            month5: "mayo",
+            month6: "junio",
+            month7: "julio",
+            month8: "agosto",
+            month9: "septiembre",
+            month10: "octubre",
+            month11: "noviembre",
+            month12: "diciembre",
+            weeks: {
+                sun: "dom",
+                mon: "lun",
+                tue: "mar",
+                wed: "mié",
+                thu: "jue",
+                fri: "vie",
+                sat: "sáb"
+            },
+            weeksFull: {
+                sun: "Domingo",
+                mon: "Lunes",
+                tue: "Martes",
+                wed: "Miércoles",
+                thu: "Jueves",
+                fri: "Viernes",
+                sat: "Sábado"
+            },
+            months: {
+                jan: "ene",
+                feb: "feb",
+                mar: "mar",
+                apr: "abr",
+                may: "may",
+                jun: "jun",
+                jul: "jul",
+                aug: "ago",
+                sep: "sep",
+                oct: "oct",
+                nov: "nov",
+                dec: "dic"
+            }
+        },
+        inputNumber: {
+            decrease: "disminuir número",
+            increase: "aumentar número"
+        },
+        select: {
+            loading: "Cargando",
+            noMatch: "No hay datos que coincidan",
+            noData: "Sin datos",
+            placeholder: "Seleccionar"
+        },
+        mention: { loading: "Cargando" },
+        dropdown: { toggleDropdown: "Alternar menú desplegable" },
+        cascader: {
+            noMatch: "No hay datos que coincidan",
+            loading: "Cargando",
+            placeholder: "Seleccionar",
+            noData: "Sin datos"
+        },
+        pagination: {
+            goto: "Ir a",
+            pagesize: "/página",
+            total: "Total {total}",
+            pageClassifier: "",
+            page: "Página",
+            prev: "Ir a la página anterior",
+            next: "Ir a la página siguiente",
+            currentPage: "página {pager}",
+            prevPages: "{pager} páginas anteriores",
+            nextPages: "{pager} páginas siguientes",
+            deprecationWarning: "Se detectaron usos obsoletos; consulta la documentación de el-pagination para obtener más detalles"
+        },
+        dialog: { close: "Cerrar este diálogo" },
+        drawer: { close: "Cerrar este panel" },
+        messagebox: {
+            title: "Mensaje",
+            confirm: "Aceptar",
+            cancel: "Cancelar",
+            error: "Entrada inválida",
+            close: "Cerrar este diálogo"
+        },
+        upload: {
+            deleteTip: "Pulse Eliminar para retirar",
+            delete: "Eliminar",
+            preview: "Vista Previa",
+            continue: "Continuar"
+        },
+        slider: {
+            defaultLabel: "control deslizante entre {min} y {max}",
+            defaultRangeStartLabel: "seleccionar el valor inicial",
+            defaultRangeEndLabel: "seleccionar el valor final"
+        },
+        table: {
+            emptyText: "Sin Datos",
+            confirmFilter: "Confirmar",
+            resetFilter: "Reiniciar",
+            clearFilter: "Todos",
+            sumText: "Suma",
+            selectAllLabel: "Seleccionar todas las filas",
+            selectRowLabel: "Seleccionar esta fila",
+            expandRowLabel: "Expandir esta fila",
+            collapseRowLabel: "Contraer esta fila",
+            sortLabel: "Ordenar por {column}",
+            filterLabel: "Filtrar por {column}"
+        },
+        tag: { close: "Cerrar esta etiqueta" },
+        tour: {
+            next: "Siguiente",
+            previous: "Anterior",
+            finish: "Finalizar",
+            close: "Cerrar"
+        },
+        tree: { emptyText: "Sin Datos" },
+        transfer: {
+            noMatch: "No hay datos que coincidan",
+            noData: "Sin datos",
+            titles: ["Lista 1", "Lista 2"],
+            filterPlaceholder: "Ingresar palabra clave",
+            noCheckedFormat: "{total} artículos",
+            hasCheckedFormat: "{checked}/{total} revisados"
+        },
+        image: { error: "HA FALLADO" },
+        pageHeader: { title: "Volver" },
+        popconfirm: {
+            confirmButtonText: "Sí",
+            cancelButtonText: "No"
+        },
+        carousel: {
+            leftArrow: "Flecha izquierda del carrusel",
+            rightArrow: "Flecha derecha del carrusel",
+            indicator: "Cambiar el carrusel al índice {index}"
+        },
+        inputOTP: {
+            groupLabel: "Entrada OTP",
+            defaultLabel: "Introduce el carácter OTP {index}"
+        }
+    }
+}
--- stubs/localization/resources/js/locales/pt.js
+++ stubs/localization/resources/js/locales/pt.js
@@ -1,1 +1,180 @@
-export { default } from 'element-plus/es/locale/lang/pt'
+export default {
+    name: "pt",
+    el: {
+        breadcrumb: { label: "Navegação estrutural" },
+        colorpicker: {
+            confirm: "Confirmar",
+            clear: "Limpar",
+            defaultLabel: "seletor de cores",
+            description: "a cor atual é {color}. prima Enter para selecionar uma nova cor.",
+            alphaLabel: "selecionar o valor alfa",
+            alphaDescription: "alfa {alpha}, a cor atual é {color}",
+            hueLabel: "selecionar o valor de matiz",
+            hueDescription: "matiz {hue}, a cor atual é {color}",
+            svLabel: "selecionar os valores de saturação e brilho",
+            svDescription: "saturação {saturation}, brilho {brightness}, a cor atual é {color}",
+            predefineDescription: "selecionar {value} como cor"
+        },
+        datepicker: {
+            now: "Agora",
+            today: "Hoje",
+            cancel: "Cancelar",
+            clear: "Limpar",
+            confirm: "Confirmar",
+            dateTablePrompt: "Utilize as teclas de seta e Enter para selecionar o dia do mês",
+            monthTablePrompt: "Utilize as teclas de seta e Enter para selecionar o mês",
+            yearTablePrompt: "Utilize as teclas de seta e Enter para selecionar o ano",
+            selectedDate: "Data selecionada",
+            selectDate: "Selecionar data",
+            selectTime: "Selecionar hora",
+            startDate: "Data de início",
+            startTime: "Hora de início",
+            endDate: "Data de fim",
+            endTime: "Hora de fim",
+            prevYear: "Ano anterior",
+            nextYear: "Ano seguinte",
+            prevMonth: "Mês anterior",
+            nextMonth: "Mês seguinte",
+            year: "Ano",
+            month1: "Janeiro",
+            month2: "Fevereiro",
+            month3: "Março",
+            month4: "Abril",
+            month5: "Maio",
+            month6: "Junho",
+            month7: "Julho",
+            month8: "Agosto",
+            month9: "Setembro",
+            month10: "Outubro",
+            month11: "Novembro",
+            month12: "Dezembro",
+            weeks: {
+                sun: "Dom",
+                mon: "Seg",
+                tue: "Ter",
+                wed: "Qua",
+                thu: "Qui",
+                fri: "Sex",
+                sat: "Sáb"
+            },
+            weeksFull: {
+                sun: "Domingo",
+                mon: "Segunda-feira",
+                tue: "Terça-feira",
+                wed: "Quarta-feira",
+                thu: "Quinta-feira",
+                fri: "Sexta-feira",
+                sat: "Sábado"
+            },
+            months: {
+                jan: "Jan",
+                feb: "Fev",
+                mar: "Mar",
+                apr: "Abr",
+                may: "Mai",
+                jun: "Jun",
+                jul: "Jul",
+                aug: "Ago",
+                sep: "Set",
+                oct: "Out",
+                nov: "Nov",
+                dec: "Dez"
+            }
+        },
+        inputNumber: {
+            decrease: "diminuir número",
+            increase: "aumentar número"
+        },
+        select: {
+            loading: "A carregar",
+            noMatch: "Sem correspondência",
+            noData: "Sem dados",
+            placeholder: "Selecione"
+        },
+        mention: { loading: "A carregar" },
+        dropdown: { toggleDropdown: "Alternar menu pendente" },
+        cascader: {
+            noMatch: "Sem correspondência",
+            loading: "A carregar",
+            placeholder: "Selecione",
+            noData: "Sem dados"
+        },
+        pagination: {
+            goto: "Ir para",
+            pagesize: "/página",
+            total: "Total {total}",
+            pageClassifier: "",
+            page: "Página",
+            prev: "Ir para a página anterior",
+            next: "Ir para a página seguinte",
+            currentPage: "página {pager}",
+            prevPages: "{pager} páginas anteriores",
+            nextPages: "{pager} páginas seguintes",
+            deprecationWarning: "Foram detetadas utilizações obsoletas; consulte a documentação do el-pagination para obter mais detalhes"
+        },
+        dialog: { close: "Fechar esta caixa de diálogo" },
+        drawer: { close: "Fechar este painel" },
+        messagebox: {
+            title: "Mensagem",
+            confirm: "Confirmar",
+            cancel: "Cancelar",
+            error: "Erro!",
+            close: "Fechar esta caixa de diálogo"
+        },
+        upload: {
+            deleteTip: "prima Delete para remover",
+            delete: "Apagar",
+            preview: "Previsualizar",
+            continue: "Continuar"
+        },
+        slider: {
+            defaultLabel: "controlo deslizante entre {min} e {max}",
+            defaultRangeStartLabel: "selecionar o valor inicial",
+            defaultRangeEndLabel: "selecionar o valor final"
+        },
+        table: {
+            emptyText: "Sem dados",
+            confirmFilter: "Confirmar",
+            resetFilter: "Limpar",
+            clearFilter: "Todos",
+            sumText: "Total",
+            selectAllLabel: "Selecionar todas as linhas",
+            selectRowLabel: "Selecionar esta linha",
+            expandRowLabel: "Expandir esta linha",
+            collapseRowLabel: "Recolher esta linha",
+            sortLabel: "Ordenar por {column}",
+            filterLabel: "Filtrar por {column}"
+        },
+        tag: { close: "Fechar esta etiqueta" },
+        tour: {
+            next: "Seguinte",
+            previous: "Anterior",
+            finish: "Concluir",
+            close: "Fechar"
+        },
+        tree: { emptyText: "Sem dados" },
+        transfer: {
+            noMatch: "Sem correspondência",
+            noData: "Sem dados",
+            titles: ["Lista 1", "Lista 2"],
+            filterPlaceholder: "Introduza uma palavra-chave",
+            noCheckedFormat: "{total} itens",
+            hasCheckedFormat: "{checked}/{total} selecionados"
+        },
+        image: { error: "Falha ao carregar a imagem" },
+        pageHeader: { title: "Voltar" },
+        popconfirm: {
+            confirmButtonText: "Sim",
+            cancelButtonText: "Não"
+        },
+        carousel: {
+            leftArrow: "Seta esquerda do carrossel",
+            rightArrow: "Seta direita do carrossel",
+            indicator: "Mudar o carrossel para o índice {index}"
+        },
+        inputOTP: {
+            groupLabel: "Entrada OTP",
+            defaultLabel: "Introduza o caráter OTP {index}"
+        }
+    }
+}
